c09b9810d5c845e767402adf88960fae02c3f3ef
Mengxun326 <Meng__xun@163.com>
2025-04-30 01:30:43 +0800

    更新mybatis-plus
    2025.4.30

--- src/main/java/com/mengxun/ticketbookingbackend/TicketBookingBackendApplication.java
+++ src/main/java/com/mengxun/ticketbookingbackend/TicketBookingBackendApplication.java
@@ -1,9 +1,11 @@
 package com.mengxun.ticketbookingbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.mengxun.ticketbookingbackend.mapper")
 public class TicketBookingBackendApplication {
 
     public static void main(String[] args) {
