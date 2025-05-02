fedf29572b4e1de3d36000576bcaa1ff9aaac54b
Mengxun326 <Meng__xun@163.com>
2025-05-02 20:24:34 +0800

    添加UserMapper
    2025.5.2

---
+++ src/main/java/com/mengxun/ticketbookingbackend/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.mengxun.ticketbookingbackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mengxun.ticketbookingbackend.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
