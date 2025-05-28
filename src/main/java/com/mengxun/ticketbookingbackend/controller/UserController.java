041909c4c9920ce7824f1f4ad2478fed12bea7db
Mengxun326 <Meng__xun@163.com>
2025-05-28 15:17:13 +0800

    Update UserController.java

--- src/main/java/com/mengxun/ticketbookingbackend/controller/UserController.java
+++ src/main/java/com/mengxun/ticketbookingbackend/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.mengxun.ticketbookingbackend.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mengxun.ticketbookingbackend.model.domain.User;
+import com.mengxun.ticketbookingbackend.model.domain.request.UserLoginRequest;
 import com.mengxun.ticketbookingbackend.model.domain.request.UserRegisterRequest;
 import com.mengxun.ticketbookingbackend.service.UserService;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.mengxun.ticketbookingbackend.constant.UserConstant.ADMIN_ROLE;
+import static com.mengxun.ticketbookingbackend.constant.UserConstant.USER_LOGIN_STATE;
 
 /**
  * 用户接口
@@ -35,4 +42,62 @@ public class UserController {
         }
         return userService.userRegister(userAccount, userPassword, checkPassword);
     }
+
+    @PostMapping("/login")
+    public User userLogin(@RequestBody UserLoginRequest userRegisterRequest, HttpServletRequest request){
+        if(userRegisterRequest == null)
+        {
+            return null;
+        }
+        String userAccount = userRegisterRequest.getUserAccount();
+        String userPassword = userRegisterRequest.getUserPassword();
+        if(StringUtils.isAnyBlank(userAccount,userPassword))
+        {
+            return null;
+        }
+        return userService.userLogin(userAccount, userPassword,request);
+    }
+
+    @GetMapping("/search")
+    public List<User> searchUsers(String username,HttpServletRequest request){
+        //仅管理员可查询
+        if(!isAdmin(request))
+        {
+            return new ArrayList<>();
+        }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        if(StringUtils.isNotBlank(username))
+        {
+            queryWrapper.like("username",username);
+        }
+        List<User> userlist = userService.list(queryWrapper);
+        return userlist.stream().map(user -> userService.getSafetyUser(user)).collect(Collectors.toList());
+    }
+
+    @PostMapping("/delete")
+    public boolean deleteUser(@RequestBody long id,HttpServletRequest request){
+        //仅管理员可删除
+        if(!isAdmin(request))
+        {
+            return false;
+        }
+        if(id<=0)
+        {
+            return false;
+        }
+        return userService.removeById(id);
+    }
+
+    /**
+     * 是否为管理员
+     * @param request
+     * @return
+     */
+    private  boolean isAdmin(HttpServletRequest request)
+    {
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User user = (User) userObj;
+        return user != null && user.getUserrole() == ADMIN_ROLE;
+    }
+
 }
